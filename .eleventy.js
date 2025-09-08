69f1fc1416f521be94faede9cfcb962be8361207
Sven Sewitz <sven@svens-m1-mini.speedport.ip>
2025-09-08 18:31:46 +0200

    Added post on Chromatin Dynamics conference

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,27 @@
+const MarkdownIt = require("markdown-it");
+
 module.exports = function(eleventyConfig) {
   // Copy static assets if you add any later
   eleventyConfig.addPassthroughCopy({ "public": "/" });
-  eleventyConfig.addShortcode("year", () => new Date().getFullYear());
+  eleventyConfig.addPassthroughCopy({ "src/images": "images" });
   
+  // Shortcode: current year
+  eleventyConfig.addShortcode("year", () => new Date().getFullYear());
+
+  // Shortcode: render Markdown inside .njk files
+  const md = new MarkdownIt({ html: true, breaks: true, linkify: true });
+  eleventyConfig.addPairedShortcode("md", (content) => md.render(content));
+
+  // Shortcode: figure with caption
+  eleventyConfig.addPairedShortcode("figure", (content, caption) => {
+    return `
+      <figure class="my-6">
+        ${content}
+        <figcaption class="mt-2 text-sm italic text-slate-600">${caption}</figcaption>
+      </figure>
+    `;
+  });
+
   return {
     dir: { 
       input: "src", 
